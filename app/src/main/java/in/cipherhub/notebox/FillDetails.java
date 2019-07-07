89423b8b72f0e4dd9921a85526978ef04a7b42d7
065rsh <gogl.arshdeep@gmail.com>
2019-07-07 23:48:10 +0530

    playing with PR

--- app/src/main/java/in/cipherhub/notebox/FillDetails.java
+++ app/src/main/java/in/cipherhub/notebox/FillDetails.java
@@ -1,9 +1,7 @@
 package in.cipherhub.notebox;
 
-import android.content.Context;
-import android.net.Uri;
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- app/src/main/java/in/cipherhub/notebox/Profile.java
+++ app/src/main/java/in/cipherhub/notebox/Profile.java
@@ -31,34 +31,9 @@ public class Profile extends Fragment implements View.OnClickListener {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-//        storage = FirebaseStorage.getInstance();
-//
-//        httpsReference = FirebaseStorage.getInstance().getReference();
-//
-//        Button download_pdf = view.findViewById(R.id.download_pdf);
-//        download_pdf.setOnClickListener(this);
 
-        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-
-        // Get reference to the file
-        StorageReference forestRef = storageRef.child("trial/trial.pdf");
-
-        Log.d(TAG, "Opened Profile page");
-
-        forestRef.getMetadata().addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
-            @Override
-            public void onSuccess(StorageMetadata storageMetadata) {
-                // Metadata now contains the metadata for 'images/forest.jpg'
-                Log.d(TAG, "here" + storageMetadata);
-                Log.d(TAG, storageMetadata.getCustomMetadata("name"));
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception exception) {
-                // Uh-oh, an error occurred!
-                Log.d(TAG, "Failed to get Metadata: " + exception);
-            }
-        });
+        Button download_pdf = rootView.findViewById(R.id.download_pdf);
+        download_pdf.setOnClickListener(this);
 
         return rootView;
     }
@@ -66,36 +41,36 @@ public class Profile extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-//        switch (v.getId()) {
-//
-//            case R.id.download_pdf:
-//
-//                // Create our main directory for storing files
-//                File directPath = new File(Environment.getExternalStorageDirectory() + "/Notebox");
-//                if (!directPath.exists()) {
-//                    boolean mkdir = directPath.mkdir();
-//                    if (!mkdir) {
-//                        Log.e(TAG, "Directory creation failed.");
-//                    }
-//                }
-//
-//                Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/notebox-1559903149503.appspot.com/o/CSE%2FPYTHON%2FNumpy%20Exercises%20(Unit-1).pdf?alt=media&token=daa8d108-24c4-482b-b4c0-d5a6fb9e15df");
-//
-//                DownloadManager mgr = (DownloadManager) Objects.requireNonNull(
-//                        getActivity()).getSystemService(Context.DOWNLOAD_SERVICE);
-//
-//                DownloadManager.Request request = new DownloadManager.Request(uri);
-//                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
-//                        .setAllowedOverRoaming(false)
-//                        // can be named dynamically once we have database ready
-////                        .setTitle("test1.pdf")
-////                        .setDescription("Something useful? Maybe.")
-//                        .setDestinationInExternalPublicDir("/Notebox", "test.pdf")
-//                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-//
-//                mgr.enqueue(request);
-//
-//        }
+        switch (v.getId()) {
+
+            case R.id.download_pdf:
+
+                // Create our main directory for storing files
+                File directPath = new File(Environment.getExternalStorageDirectory() + "/Notebox");
+                if (!directPath.exists()) {
+                    boolean mkdir = directPath.mkdir();
+                    if (!mkdir) {
+                        Log.e(TAG, "Directory creation failed.");
+                    }
+                }
+
+                Uri uri = Uri.parse("https://firebasestorage.googleapis.com/v0/b/notebox-1559903149503.appspot.com/o/CSE%2FPYTHON%2FNumpy%20Exercises%20(Unit-1).pdf?alt=media&token=daa8d108-24c4-482b-b4c0-d5a6fb9e15df");
+
+                DownloadManager mgr = (DownloadManager) Objects.requireNonNull(
+                        getActivity()).getSystemService(Context.DOWNLOAD_SERVICE);
+
+                DownloadManager.Request request = new DownloadManager.Request(uri);
+                request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request.NETWORK_MOBILE)
+                        .setAllowedOverRoaming(false)
+                        // can be named dynamically once we have database ready
+//                        .setTitle("test1.pdf")
+//                        .setDescription("Something useful? Maybe.")
+                        .setDestinationInExternalPublicDir("/Notebox", "test.pdf")
+                        .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                mgr.enqueue(request);
+
+        }
     }
 }
   
